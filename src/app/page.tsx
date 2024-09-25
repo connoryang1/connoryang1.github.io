14f13574a4bdbc5c0f9b32807bf76b1daba6a0a2
Connor Yang <connoryang2005@gmail.com>
2024-09-25 12:27:22 -0400

    Adjust loading screen time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 5000);
+    }, 500);
 
     return () => clearTimeout(timer);
   }, []);
