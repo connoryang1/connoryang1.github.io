58e577cafe0661c0a565a6844e8ae6772c46481d
Connor Yang <connoryang2005@gmail.com>
2024-09-26 13:51:05 -0400

    Change path

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/",
+  basePath: "",
 
   /**
    * Disable server-based image optimization. Next.js does not support
@@ -23,7 +23,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/',
+  assetPrefix: "",
 };
 
 export default nextConfig;
