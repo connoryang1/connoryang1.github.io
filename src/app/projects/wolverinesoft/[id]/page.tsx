e5b1da5603f3a3b3e2ecf974f9650deb91e71c54
Connor Yang <connoryang2005@gmail.com>
2024-09-26 12:28:41 -0400

    Change return

--- src/app/projects/wolverinesoft/[id]/page.tsx
+++ src/app/projects/wolverinesoft/[id]/page.tsx
@@ -1,14 +1,14 @@
 export async function generateStaticParams() {
-  return {
-    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
-  }}
+  return [
+    { params: { id: '1' } },
+    { params: { id: '2' } },
+  ];
+}
 
-export default function Page() {
+export default function Page({ params }: { params: { id: string } }) {
   return (
-    <div style={{
-      color: "white",
-    }}>
-      <h1>Dev Blog</h1>
+    <div style={{ color: "white" }}>
+      <h1>Dev Blog for Project {params.id}</h1>
     </div>
   );
 }
