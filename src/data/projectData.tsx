fb5753b7257f7f09bcebfaf9a7f1b864165dad5c
Connor Yang <connoryang2005@gmail.com>
2024-09-26 14:32:26 -0400

    Add mclubs images

--- src/data/projectData.tsx
+++ src/data/projectData.tsx
@@ -1,3 +1,4 @@
+import mclubs from "@/assets/mclubs.webp";
 import wolverineSoft from "@/assets/wolverinesoft.webp";
 import DesktopBackground from "@/components/portfolio-desktop/DesktopBackground";
 import Image from "next/image";
@@ -6,15 +7,17 @@ const projectData = [
   {
     title: "WolverineSoft",
     description: "DevBlog for WolverineSoft Studio.",
-    content: <div>Hi<Image src={wolverineSoft.src} alt="WolverineSoft Logo" fill={true} style={{
+    content: <Image src={wolverineSoft.src} alt="WolverineSoft Logo" fill={true} style={{
       objectFit: "cover",
-    }}/></div>,
+    }}/>,
     link: "/projects/wolverinesoft",
   },
   {
     title: "M-Clubs",
     description: "A club management app for the University of Michigan.",
-    content: <DesktopBackground />,
+    content: <Image src={mclubs.src} alt="M-Clubs Brochure" fill={true} style={{
+      objectFit: "cover",
+    }}/>,
     link: "/projects/mclubs",
     technologies: ["React Native", "Firebase", "Expo"],
   },
