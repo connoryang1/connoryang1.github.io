d14f722a2f7201da06d1bb05efd44112f1763861
Connor Yang <connoryang2005@gmail.com>
2024-10-14 00:14:46 -0400

    Reupload devblog 1 with images and updated details

--- src/app/projects/wolverinesoft/devblog1/page.tsx
+++ src/app/projects/wolverinesoft/devblog1/page.tsx
@@ -1,4 +1,8 @@
 import styles from "@/app/projects/wolverinesoft/devblog1/page.module.css";
+import Image from "next/image";
+import newimg from "./new.png";
+import prev from "./prev.png";
+import snaptogrid from "./snaptogrid.png";
 
 export default function Page() {
   return (
@@ -39,9 +43,12 @@ export default function Page() {
       <div>
         <p>
           <strong>Studio Meeting 2:</strong> (2 hours) <br />
-          In this meeting, I was assigned to fix the wall system alongside Ishita. We discussed potential issues, solutions, and explored available documentation. We found some relevant information from &quot;Base Management&quot; on wall implementation, but there was not too much to go off of.
+          In this meeting, I was assigned to fix the wall system alongside Ishita.
+          We began by testing the current wall system in the game, discussing potential issues, solutions, and exploring available documentation.
+          We found some relevant information from &quot;Base Management&quot; on wall implementation, but there was not too much to go off of.
           In the end, we were able to come up with a list of a few possible changes we could start with:
         </p>
+          <Image src={prev.src} alt="Wall System" width={600} height={300} />
         <ul>
           <li>Snapping walls to a grid layout - at least in 4 directions, but possibly allow functionaly for up to 45 degree angles</li>
           <li>Removing the side towers/replacing them with a separate building</li>
@@ -60,17 +67,22 @@ export default function Page() {
       <p>
         I began the implementation phase by removing the side towers from the current system to simplify wall construction.
         I created a <code>SnapToGrid</code> function to align the walls to a grid.
+      </p>
+
+      <Image src={snaptogrid.src} alt="Snap To Grid" width={600} height={300} />
+      <p>
         By wrapping a few existing functions with this new functionality, I was able to start snapping some parts of the wall-building process.
         Immediately an issue arose though, in the fact that while the start and end locations were snapping, the walls themselves were not.
         In addition, since the wall position and rotation calculations were under the assumption that the start and end markers faced each other, I had to adjust this so that the constructued wall would be placed in the correct spot.
         In the end, I managed to get a working prototype of a grid system for wall placement.
-        In total, this process took about 2.5 hours.
+        In total, this process took about 3.5 hours.
       </p>
+
+      <Image src={newimg.src} alt="New Wall System" width={600} height={300} />
 
       <h2>Summary of Hours</h2>
       <ul>
         <li>Studio Meeting 1: 2 hours</li>
-        <li>Programming Onboarding: 2 hours</li>
         <li>Confluence Reading: 15 minutes</li>
         <li>Debugger Setup: 30 minutes</li>
         <li>Game Playthrough: 15 minutes</li>
@@ -78,7 +90,7 @@ export default function Page() {
         <li>Codebase Exploration: 30 minutes</li>
         <li>Studio Meeting 2: 2 hours</li>
         <li>Week 1 Research: 1 hour</li>
-        <li>Week 2 Implementation: 1.5 hours</li>
+        <li>Week 2 Implementation: 3.5 hours</li>
       </ul>
 
       <p><strong>Total Hours Spent:</strong> 10 hours, 30 minutes</p>
