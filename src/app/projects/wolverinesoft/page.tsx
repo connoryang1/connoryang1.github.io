61a105c0ff4037ae76dd1da455a4c61a017a0c5e
Connor Yang <connoryang2005@gmail.com>
2024-10-14 00:20:58 -0400

    Why is wolverinesoft page broken

--- src/app/projects/wolverinesoft/page.tsx
+++ src/app/projects/wolverinesoft/page.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <ol>
-      <Link href="/projects/wolverinesoft/devblog1">Devblog 1: September 29th - October 13th</Link>
-    </ol>
+    <Link href="/projects/wolverinesoft/devblog1">Devblog 1: September 29th - October 13th</Link>
   );
 }
